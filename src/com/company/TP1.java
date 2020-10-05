9ef5e6a6036c5a4918f4fd1b20aa9a8e54886395
Nohan75 <nohanmarielouise@gmail.com>
2020-10-05 15:55:03 +0200

    Commit du TP #01

---
+++ src/com/company/TP1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class TP1 {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int[] scores = {25, 56, 93, 37, 24, 1, 78};
+        float total = 0f;
+        
+
+        for(int i = 0; i < scores.length; i++){
+            System.out.println(scores[i]);
+            total = total + scores[i];
+        }
+        float moyenne = total / scores.length;
+        System.out.println("Moyenne: " + moyenne);
+
+    }
+}
