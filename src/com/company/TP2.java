d294f6c0d5e71f3737b24521807b9f7aa5466fd2
Nohan75 <nohanmarielouise@gmail.com>
2020-10-05 16:26:39 +0200

    TP #02

--- src/com/company/TP2.java
+++ src/com/company/TP2.java
@@ -1,4 +1,44 @@
 package com.company;
 
 public class TP2 {
+
+    static void scores(int[] score){
+        for(int i = 0; i < score.length; i++){
+            System.out.println(score[i]);
+        }
+    }
+
+    static int max(int[] scores){
+        int maxScore = scores[0];
+        for(int i = 0; i < scores.length; i++){
+            if (maxScore < scores[i]) {
+                maxScore = scores[i];
+            }
+        }
+        return maxScore;
+    }
+
+    static boolean inferieurDix(int[] scores){
+        boolean isInferieur = false;
+        for(int i = 0; i < scores.length; i++){
+            if(scores[i] < 10){
+                isInferieur = true;
+            }
+        }
+        return isInferieur;
+    }
+
+    public static void main(String[] args) {
+        int[] score = {25, 100, 93, 37, 24, 1, 300};
+
+        scores(score);
+        System.out.println("-----");
+        System.out.println("Le score max est " + max(score));
+        System.out.println("-----");
+        if(inferieurDix(score)){
+            System.out.println("Au moins un score est inférieur à 10");
+        } else {
+            System.out.println("Aucun score n'est inférieur à 10");
+        }
+    }
 }
