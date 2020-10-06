1b1030433541db39ad998e6399471fe5226f6af5
Nohan75 <nohanmarielouise@gmail.com>
2020-10-06 14:27:07 +0200

    TP #04

--- src/com/company/TP4.java
+++ src/com/company/TP4.java
@@ -1,4 +1,39 @@
 package com.company;
 
 public class TP4 {
+
+
+    /**
+     * Vérifie si le nombre recherché se trouve dans le tableau et retourne son index
+     * @param tab le tableau à parcourir
+     * @param number le nombre à trouver
+     * @return l'index du numbre dans le tableau
+     */
+    public static int check(int[] tab, int number){
+        int i = -1;
+        int low = 0;
+        int max = tab.length - 1;
+
+
+        while(low <= max){
+            int mid = (low + max) / 2;
+
+            if(tab[mid] < number){
+                low = mid+1;
+            } else if(tab[mid] > number){
+                max = mid-1;
+            } else if(tab[mid] == number){
+                i = mid;
+                break;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+
+        int[] tab = {1, 2, 3, 4, 5, 6, 7};
+
+        System.out.println(check(tab, 4));
+    }
 }
